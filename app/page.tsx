1eb7789b69b2035553e1e25f1b47bbff31ede08e
Niharika <Niharika@zoikogroup.com>
2026-08-13 16:41:22 +0530

    meta data added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import HomePage from "./home/page";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Secure Video Streaming & Live Events | ZoikoStream'
+};
 export default function page(){
 return (
   <HomePage/>
